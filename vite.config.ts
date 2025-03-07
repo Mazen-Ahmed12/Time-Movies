50d5eb72d44fd27a95e53dcc7955f95ed2b9f28b
mazen ahmed <46138799+Mazen-Ahmed12@users.noreply.github.com>
2025-03-07 22:12:18 +0200

    the deployment done

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: "/watch-bay/",
+  base: "/Time-Movies/",
   optimizeDeps: {
     exclude: ["sw.js"], // Exclude the problematic file
   },
